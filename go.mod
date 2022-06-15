87de721705a1330dc9bd4fdaa9979dcd9c26d831
Chance <885046048@qq.com>
2022-06-15 14:17:37 +0800

    feat: add the colored console package

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/gin-gonic/gin v1.7.7
+	github.com/gookit/color v1.5.1
 	github.com/smartystreets/goconvey v1.7.2
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.1
@@ -40,6 +41,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
---
+++ pkg/console/console.go
@@ -0,0 +1,70 @@
+package console
+
+import (
+	"github.com/gookit/color"
+	"os"
+)
+
+func Log(a ...interface{}) {
+	color.Note.Print(a...)
+}
+
+func Logp(format string, a ...interface{}) {
+	color.Note.Prompt(format, a...)
+}
+
+func Logln(a ...interface{}) {
+	color.Note.Println(a...)
+}
+
+func Logf(format string, a ...interface{}) {
+	color.Note.Sprintf(format, a...)
+}
+
+func Success(a ...interface{}) {
+	color.Infop(a...)
+}
+
+func Successp(format string, a ...interface{}) {
+	color.Info.Prompt(format, a...)
+}
+
+func Successln(a ...interface{}) {
+	color.Infoln(a...)
+}
+
+func Successf(format string, a ...interface{}) {
+	color.Infof(format, a...)
+}
+
+func Error(a ...interface{}) {
+	color.Errorp(a...)
+}
+
+func Errorp(format string, a ...interface{}) {
+	color.Error.Prompt(format, a...)
+}
+
+func Errorln(a ...interface{}) {
+	color.Errorln(a...)
+}
+
+func Errorf(format string, a ...interface{}) {
+	color.Errorf(format, a...)
+}
+
+func Exit(msg string) {
+	Errorp(msg)
+	os.Exit(1)
+}
+
+func ExitIf(err error) {
+	if err != nil {
+		Exit(err.Error())
+	}
+}
+
+func D(msg string) {
+	Log(msg)
+	os.Exit(1)
+}
